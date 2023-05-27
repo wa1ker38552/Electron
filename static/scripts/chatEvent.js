425455acc12c41ed113b682603dbbbf98006efb7
walker <100868154+wa1ker38552@users.noreply.github.com>
2023-05-27 13:33:56 -0700

    Update chatEvent.js

--- static/scripts/chatEvent.js
+++ static/scripts/chatEvent.js
@@ -13,3 +13,28 @@ function openLeaveModal() {
 function closeLeaveModal() {
   leaveModal.style.display = "none"
 }
+
+function sendMessage(id, message) {
+  fetch(`/chats/${id}/message`, {
+    method : "POST",
+    body: JSON.stringify({content: message})
+  })
+    .then(response => response.json())
+    .then(response => {
+      console.log(response)
+    })
+}
+
+function setupSreamManager(millis, id) {
+  setInterval(async function() {
+    const p = document.getElementsByClassName("chat-content")[0]
+    const a = await fetch("/stream/"+id)
+    const b = await a.json()
+
+    if (b.data.length > 0) {
+      for (message of b.data) {
+        p.append(createMessage(message))
+      }
+    }
+  }, millis)
+}
